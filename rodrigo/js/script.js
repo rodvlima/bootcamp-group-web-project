e10b90b9159438a9318ab9cf637edf1daf48e1a1
rodvlima <39539040+rodvlima@users.noreply.github.com>
2018-06-13 13:15:30 -0400

    defining click event

--- rodrigo/js/script.js
+++ rodrigo/js/script.js
@@ -5,19 +5,27 @@ logo.src = 'img/logo1.jpg';
 
 const container = document.createElement('div');
 container.setAttribute('class','container');
+container.setAttribute('id','container');
 
 app.appendChild(logo);
 app.appendChild(container);
 
+const apiAddress = 'http://gateway.marvel.com/v1/public/';
+const getType = 'characters';
+const apikey = '766611ab74f4e8ab5d5b29c5f6e7d398';
+const hash = 'aea69f12a4b31ababd0c88e37b488550';
+const ts = '1';
+//var modifiedSince = '2010-01-01';
+const limit = 100;
+const offset = 1000;
+let nameStartsWith = '';
+const conf = '&limit='+limit+'&offset='+offset;
+
+var validation = '?apikey='+apikey+'&hash='+hash+'&ts='+ts;
+var api = apiAddress+getType+validation+conf
+console.log(api);
 //open a new connection using the get resquest
-fetch('http://gateway.marvel.com/v1/public/characters?'+
-'apikey=766611ab74f4e8ab5d5b29c5f6e7d398'+
-'&hash=aea69f12a4b31ababd0c88e37b488550'+
-'&ts=1'+
-'&limit=100'+
-'&modifiedSince=2010-01-01'+
-'&offset=100'
-)
+fetch(api)
 //convert the response to json
 .then(function(response){
     return response.json();
@@ -27,7 +35,7 @@ fetch('http://gateway.marvel.com/v1/public/characters?'+
     data = data.data.results;
     for(var i in data){
         const card = `
-            <div class="card">
+            <div class="card" id=${data[i].id}>
                 <h1>${data[i].name}</h1>
                 <img src=${data[i].thumbnail.path}/portrait_xlarge.${data[i].thumbnail.extension}>
                 <p>${data[i].description}</p>
@@ -37,5 +45,13 @@ fetch('http://gateway.marvel.com/v1/public/characters?'+
             container.innerHTML += card;
         }
     }
-    
+    var children = document.getElementsByClassName("card");
+    for(var i = 0; i < children.length; i++){
+        const idCharacter = document.getElementById(children[i].id);
+        var listener = document.getElementById(idCharacter.id).addEventListener("click",function(){
+            console.log(idCharacter.id);
+    });
+}
 })
+
+

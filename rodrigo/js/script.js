babc56ee842b6d9aa3c837aea3782551326d31bd
rodvlima <39539040+rodvlima@users.noreply.github.com>
2018-06-15 00:53:53 -0400

    scroll function

--- rodrigo/js/script.js
+++ rodrigo/js/script.js
@@ -3,8 +3,15 @@ createMainPage();
 function createMainPage(){
 
     const app = document.getElementById('root');
+    
     const logo = document.createElement('img');
     logo.src = 'img/logo1.jpg';
+    
+    const spam = document.createElement('spam');
+    spam.setAttribute('class','temp');
+    spam.setAttribute('id','temp');
+    spam.setAttribute('offset','100');
+    spam.setAttribute('scroll','');
     
     const container = document.createElement('div');
     container.setAttribute('class','container');
@@ -13,18 +20,31 @@ function createMainPage(){
     const character = document.createElement('div');
     character.setAttribute('class','character');
     character.setAttribute('id','character');
-    character.style = 'display:none';
+    character.style.display = 'none';
     
-    // app.innerHTML = "";
     app.appendChild(logo);
+    app.appendChild(spam);
     app.appendChild(container);
     app.appendChild(character);
     
-    loadApi(null,null);
+    window.onscroll = function(){
+        if(character.style.display == 'none'){
+            if((window.innerHeight+this.window.scrollY) >= this.document.body.scrollHeight){
+                scrollBottom();
+            }
+        }
+    }
+    loadApi(null,null,null);
 };  
 
-function loadApi(type,value){
-    const api = apiAddress(type,value);
+function scrollBottom(){
+    const offsetId = document.getElementById('temp').getAttribute('offset');
+    document.getElementById('temp').setAttribute('offset',parseInt(offsetId)+100);
+    loadApi(null,null,offsetId);
+};
+
+function loadApi(type,value,offset){
+    const api = apiAddress(type,value,offset);
     //open a new connection using the get resquest
     fetch(api)
     //convert the response to json
@@ -37,17 +57,18 @@ function loadApi(type,value){
     })
 };
 
-function apiAddress(type,value){
+function apiAddress(type,value,offset){
     const apiAddress = 'http://gateway.marvel.com/v1/public/';
     const getType = 'characters';
     const apikey = '766611ab74f4e8ab5d5b29c5f6e7d398';
     const hash = 'aea69f12a4b31ababd0c88e37b488550';
     const ts = '1';
     const limit = 100;
-    const offset = 1000;
+    const orderBy = 'name';
+    // const offset = offset;
     const nameStartsWith = '';
     let parameters = (type == 'character'?'/'+value:'');
-    parameters += '?limit='+limit+'&offset='+offset;
+    parameters += '?orderBy='+orderBy+'&limit='+limit+'&offset='+offset;
     
     const validation = '&apikey='+apikey+'&hash='+hash+'&ts='+ts;
     const api = apiAddress+getType+parameters+validation
@@ -60,7 +81,7 @@ function fetchMarvelList(type,value,data){
         createCard(type,i,data);
     }
     createListener(type,value);
-}
+};
 
 function createCard(type,i,data){
     const card = `
@@ -74,31 +95,40 @@ function createCard(type,i,data){
         if(type == 'character'){
             character.innerHTML = "";
             character.innerHTML += card;
-            container.style = 'display:none';
-            character.style = '';
+            container.style.display = 'none';
+            character.style.display = '';
         }else{
             container.innerHTML += card;
         }
     }
-}
+};
 
 function createListener(type,value){
     if(type=="character"){
         document.getElementById('char'+value).addEventListener("click",function(){
-            character.style = 'display:none';
-            container.style = '';
+            character.style.display = 'none';
+            container.style.display = '';
+            
+            // const scroll = document.getElementById('temp').getAttribute('scroll');
+            // console.log("scroll");
+            // console.log(scroll);
+            // window.scrollTo(0,scroll);
+            
         });
     }else{
         var children = document.getElementsByClassName("card");
         for(var i = 0;i < children.length; i++){
+
+            // document.getElementById('temp').setAttribute('scroll',window.innerHeight+this.window.scrollY+100);
+            
             const idCharacter = document.getElementById(children[i].id);
             document.getElementById(idCharacter.id).addEventListener("click",function(){
-                loadApi('character',idCharacter.id);
+                loadApi('character',idCharacter.id,null);
             });
         }
     }
-}
+};
 
-function clearBox(elementID){
+function clearElement(elementID){
     document.getElementById(elementID).innerHTML = "";
-}
+};

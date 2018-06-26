d2e0ddab816b594382e98d814070ad2945842acd
rodvlima <39539040+rodvlima@users.noreply.github.com>
2018-06-26 15:34:02 -0400

    load character in a different div

--- rodrigo/js/script.js
+++ rodrigo/js/script.js
@@ -1,11 +1,11 @@
 let offset = 0;
-let limit = 50;
+let limit = 20;
 let scroll = true;
-let windowHeight ;
+let cardPosition;
 
 (function createMainPage(){
-
-    const app = document.getElementById('root');
+    
+    const root = document.getElementById('root');
     const logo = document.createElement('img');
     logo.src = 'img/logo1.jpg';
     
@@ -13,6 +13,11 @@ let windowHeight ;
     container.setAttribute('class','container');
     container.setAttribute('id','container');
     
+    const character = document.createElement('div');
+    character.setAttribute('class','character');
+    character.setAttribute('id','character');
+    character.style.display = 'none';
+
     const preLoader = document.createElement('div');
     preLoader.setAttribute('id','preLoader');
     preLoader.setAttribute('align','center');
@@ -25,11 +30,12 @@ let windowHeight ;
     loader.style.display = 'none';
     
     preLoader.appendChild(loader);
-    app.appendChild(logo);
-    app.appendChild(container);
-    app.appendChild(preLoader);
+    root.appendChild(logo);
+    root.appendChild(container);
+    root.appendChild(character);
+    root.appendChild(preLoader);
     scrollBottom();
-    loadApi(null,null);
+    loadApi();
 })();  
 
 function scrollBottom(){
@@ -40,7 +46,7 @@ function scrollBottom(){
                 if(loading == false){
                     loader.style.display = '';
                     loading = true;
-                    loadApi(null,null);
+                    loadApi();
                 }
             }else{
                 loading = false;
@@ -49,8 +55,25 @@ function scrollBottom(){
     }
 };
 
-function loadApi(type,value){
-    const api = apiAddress(type,value);
+function apiAddress(type){
+    console.log(type)
+    const apiAddress = 'https://gateway.marvel.com/v1/public/';
+    const getType = 'characters';
+    const apikey = '766611ab74f4e8ab5d5b29c5f6e7d398';
+    const hash = 'aea69f12a4b31ababd0c88e37b488550';
+    const ts = '1';
+    const orderBy = 'name';
+    const nameStartsWith = '';
+    const validation = '?apikey='+apikey+'&hash='+hash+'&ts='+ts;
+    const parameters = '&orderBy='+orderBy+'&limit='+limit+'&offset='+offset;
+    let api = '';
+    api = apiAddress+getType+validation+parameters;
+    offset += limit;
+    return api;
+};  
+
+function loadApi(type){
+    const api = apiAddress(type);
     //open a new connection using the get resquest
     fetch(api)
     //convert the response to json
@@ -59,52 +82,72 @@ function loadApi(type,value){
     })
     //work with the json information
     .then(function(data){
-        fetchMarvelList(type,value,data)
+            fetchMarvelList(data)
     })
 };
 
-function apiAddress(type,value){
-    const apiAddress = 'https://gateway.marvel.com/v1/public/';
-    const getType = 'characters';
-    const apikey = '766611ab74f4e8ab5d5b29c5f6e7d398';
-    const hash = 'aea69f12a4b31ababd0c88e37b488550';
-    const ts = '1';
-    const orderBy = 'name';
-    const nameStartsWith = '';
-    let parameters = (type == 'character'?'/'+value:'');
-    parameters += '?orderBy='+orderBy+'&limit='+limit+'&offset='+offset;
-    
-    const validation = '&apikey='+apikey+'&hash='+hash+'&ts='+ts;
-    const api = apiAddress+getType+parameters+validation
-
-    offset += limit;
-    return api;
-};
-
-function fetchMarvelList(type,value,data){
+function fetchMarvelList(data){
     data = data.data.results;
     for(var i in data){
-        createCard(type,i,data);
+        createCard(i,data);
     }
     createListener();
 };
 
-function createCard(type,i,data){
+function fetchMarvelContents(data){
+    data = data.data.results;
+    for(var i in data){
+        createContent(i,data);
+    }
+    return data;
+};
+
+function createCard(i,data){
     const card = `
-    <div class="card" id=${(type=='character'?'char'+data[i].id:data[i].id)}>
+    <div class="card" id=${(data[i].id)}>
     <h1>${data[i].name}</h1>
     <img class="thumbnail" id=${'thumb'+data[i].id}" src=${data[i].thumbnail.path}/portrait_xlarge.${data[i].thumbnail.extension}>
     <p id="description">${data[i].description}</p>
-    <div class="content" id=${"content"+data[i].id} style="display:none;">
-    <img class="back" id=${'back'+data[i].id} src='img/button-red-back.png' width=150px; height=50px;">
-    <div>
-    </div>
-    `; // attention for this simbol: " ` "
+    </div>`; // attention for this simbol: " ` "
+
     if(data[i].description.length > 0){
         container.innerHTML += card;
         loader.style.display = 'none';
     }
 };
+
+function createCharacter(idCharacter){
+    scroll = false;
+    const character = document.getElementById('character');
+    const card = document.getElementById(idCharacter);
+    const clone = card.cloneNode(true);
+    clone.innerHTML += `<div id="content"></div>`;
+    clone.innerHTML += `<div id="back"><img id="back" src='img/button-red-back.png' width=150px; height=50px;"></div>`;
+    character.innerHTML = '';
+    character.appendChild(clone);
+    document.getElementById('back').addEventListener('click',function(){showCards()});
+    document.getElementById('container').style.display = 'none';
+    document.getElementById('preLoader').style.display = 'none';
+    character.style.display = '';
+    cardPosition = card;
+};
+
+// function createContent(i,data){
+//     const card = `
+//     <div class="card" id=${(data[i].id)}>
+//     <h1>${data[i].name}</h1>
+//     <img class="thumbnail" id=${'thumb'+data[i].id}" src=${data[i].thumbnail.path}/portrait_xlarge.${data[i].thumbnail.extension}>
+//     <p id="description">${data[i].description}</p>
+//     <div class="content" id=${"content"+data[i].id} style="display:none;">
+//     <img class="back" id=${'back'+data[i].id} src='img/button-red-back.png' width=150px; height=50px;">
+//     </div>
+//     </div>`; // attention for this simbol: " ` "
+
+//     if(data[i].description.length > 0){
+//         container.innerHTML += card;
+//         loader.style.display = 'none';
+//     }
+// };
 
 function createListener(){
     //card listener
@@ -114,52 +157,42 @@ function createListener(){
         for(var x = 0;x < card.children.length;x++){
             if(card.children[x].className == 'thumbnail'){
                 const thumb = document.getElementById(card.children[x].id);
-                thumb.addEventListener('click',function(){hideCards(card.id);});
-            }
-            if(card.children[x].className == 'content'){
-                const content = document.getElementById(card.children[x].id).getElementsByClassName('back');
-                const back = document.getElementById(content[0].id)
-                back.addEventListener('click',function(){showCards()});
+                thumb.addEventListener('click',function(){createCharacter(card.id);});
             }
         }
     }
 };
 
 function showCards(){
-    //enable spinner
     scroll = true;
-    
-    //display cards
-    var cardElement = document.getElementsByClassName('card');
-    for(var i = 0; i < cardElement.length;i++){
-        const card = document.getElementById(cardElement[i].id);
-        card.style.display = '';
-        const content = card.getElementsByClassName('content');
-        document.getElementById(content[0].id).style.display = 'none';
-    }
-    windowHeight.scrollIntoView();
-
+    document.getElementById('container').style.display = '';
+    document.getElementById('preLoader').style.display = '';
+    document.getElementById('character').style.display = 'none';
+    cardPosition.scrollIntoView();
 };
 
-function hideCards(idCharacter){
-    //disable spinner
-    scroll = false;
-    //hide others cards and remove listener
-    const cardElement = document.getElementsByClassName('card');
-    for(var i = 0; i < cardElement.length;i++){
-        const card = document.getElementById(cardElement[i].id);
-        const cardId = card.id;
-        if(cardId != idCharacter){
-            cardElement[i].style.display = 'none';
-        }else{
-            windowHeight = card;
-        }
-    }
-    //display "back" image
-    const card = document.getElementById(idCharacter);
-    for(var i = 0;i < card.children.length;i++){
-        if(card.children[i].className == 'content'){
-            card.children[i].style.display = '';
-        }
-    }
-};
+/*---- OLD ----*/
+
+
+// function hideCards(idCharacter){
+//     //disable spinner
+//     scroll = false;
+//     //hide others cards and remove listener
+//     const cardElement = document.getElementsByClassName('card');
+//     for(var i = 0; i < cardElement.length;i++){
+//         const card = document.getElementById(cardElement[i].id);
+//         const cardId = card.id;
+//         if(cardId != idCharacter){
+//             cardElement[i].style.display = 'none';
+//         }else{
+//             cardPosition = card;
+//         }
+//     }
+//     //display "back" image
+//     const card = document.getElementById(idCharacter);
+//     for(var i = 0;i < card.children.length;i++){
+//         if(card.children[i].className == 'content'){
+//             card.children[i].style.display = '';
+//         }
+//     }
+// };
